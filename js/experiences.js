6633066e45665e98ed727eeb6ab116f72ee5b40a
Farahnaz Hoque <fhoque@uci.edu>
2023-07-31 04:56:01 +0600

    Complete

---
+++ js/experiences.js
@@ -0,0 +1,52 @@
+function ml() {
+    var output = document.getElementById('output');
+    output.innerHTML = "<p style='font-size: 0.75em;'>As a Student Research Assistant in Machine Learning and Networked Systems at the University of California, Irvine's Donald Bren School of Information and Computer Sciences, I am collaborating with a Professor Marco Levarato and a dedicated team of PhD and Masters students to develop and execute avant-garde machine learning algorithms. Our cutting-edge project involves creating an augmented reality model capable of envisioning objects within a room, with meticulous consideration for the room's dimensions.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.75em;'>This innovative model is trained extensively using a multitude of furniture images, employing advanced machine learning techniques. This training enables the model to make accurate predictions in diverse room environments. The sheer size of our dataset presents a significant challenge, necessitating robust and efficient processing capabilities. Furthermore, building our machine learning models from scratch has its own complexities and intricacies. The process of refining our models has allowed us to discover and rectify potential bugs in real-time, a crucial component of any research endeavor.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.75em;'>Our team aims to complete this project by Fall 2023, with a subsequent plan to publish our findings in a detailed research paper by Winter. Despite the challenges, this project holds significant potential to contribute to the fields of machine learning and augmented reality. As we continue to refine our models and overcome challenges, we are gaining invaluable insights into algorithm development, model training, and problem-solving, further cementing our foundation in machine learning research.</p>";
+}
+
+function soar() {
+    var output = document.getElementById('output');
+    output.innerHTML = "<p style='font-size: 0.8em;'>As a Program Co-Director at the Student Outreach and Retention Centre (SOAR) at UCI, I have fostered strong relationships with SOAR affiliates. These student organizations at UCI are dedicated to supporting students from economically and ethnically disadvantaged backgrounds.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.8em;'>I have been working closely with all departments within SOAR as well as collaborating with other organizations. This inter-departmental cooperation allowed us to promote and implement strategies for the benefit of our student community. My work involves organizing events to introduce financial and academic resources aimed at improving graduation rates among minority students. I also supervise initiatives that provide vital resources to students, including the Laptop Loan Program and the Cup of SOAR initiative for free breakfasts.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.8em;'>In addition to my daily responsibilities, I've written proposals for the expansion of these programs, showcasing our continuous commitment to enhancing student resources. This role has equipped me with critical skills such as public speaking, managing budgets, using Excel for data management, and leadership. I have also gained experience in organizing meetings with interns, delegating tasks, and collaborating with the marketing team to create compelling promotional materials.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.8em;'>My experiences at SOAR have shaped my ability to effectively manage and coordinate a wide array of projects and initiatives. Learn more about our work at <a href='https://soar.uci.edu/' target='_blank'>SOAR</a>.</p>";
+}
+
+function wil() {
+    var output = document.getElementById('output');
+    output.innerHTML = "<p style='font-size: 0.9em;'>As Co-Vice President of External Affairs at Women in Leadership (WIL) at UCI, I've had the valuable opportunity to collaborate closely with another Vice President to engage with influential women across various high-profile industries and entrepreneurial ventures.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>Coordinating with our Presidents, other Vice Presidents, and members, we have striven to facilitate networking opportunities with esteemed professionals in our community. Event planning, time management, and incentivizing member participation were integral aspects of this role, and we always took into account budgetary constraints.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>Regular meetings with our interns allowed us to provide guidance and help them host successful events. Given the responsibility for most of our professional events, I developed considerable skills in networking, outreach, and negotiation. </p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>Interacting with members has also honed my ability to be a flexible team player, balancing the needs of our members with those of our guest speakers. This role has been a significant milestone in my leadership journey, offering numerous insights and experiences.</p>";
+}
+
+function mentor() {
+    var output = document.getElementById('output');
+    output.innerHTML = "<p style='font-size: 0.9em;'>As a Mentor in the Undecided/Undeclared Mentorship Program (UUMP) at UCI, I took on the responsibility of weekly check-ins with my mentees. Particularly before critical periods such as the beginning of quarters, midterms, and finals, I guided them towards planning their schedules and utilizing available online and university resources.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>Apart from individual check-ins, I arranged group hangouts post-midterms for my mentees to interact with one another, fostering a sense of community. For efficient coordination, we utilized tools like 'when2meet' and communicated primarily through email before transitioning to Instagram.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>My primary goal was to assist my mentees in navigating their degrees and exploring potential majors before declaring one. At the same time, I endeavored to provide a nurturing environment for them during their first year of college, serving as a stable support system throughout their academic journey.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>This role gave me the opportunity to exercise and develop my nurturing traits, as I took care of the freshmen students almost as if they were my own. This mentorship experience, no doubt, has significantly enhanced my empathy and understanding.</p>";
+}
+
+function la() {
+    var output = document.getElementById('output');
+    output.innerHTML = "<p style='font-size: 0.9em;'>As a Learning Assistant for Boolean Algebra at the Donald Bren School of Information and Computer Sciences, University of California, Irvine, I had the opportunity to support professors in the creation and execution of lesson plans and course materials.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>In this capacity, I adopted the role of a mentor and tutor, working one-on-one and in small groups with students to bolster their understanding of Boolean Algebra concepts. Through these interactions, I was able to guide students and clear their conceptual doubts effectively, facilitating a smoother learning experience.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>I employed a variety of teaching strategies to enrich the learning experience and engage students, including the use of visual aids and hands-on activities. These methods proved instrumental in improving student comprehension and fostering an interactive, engaging learning environment.</p>";
+
+    output.innerHTML += "<p style='font-size: 0.9em;'>The role allowed me to refine my pedagogical skills and provided insight into the various ways students learn. It also enabled me to play a critical role in their academic journey, thereby contributing positively to the broader educational community.</p>";
+}
